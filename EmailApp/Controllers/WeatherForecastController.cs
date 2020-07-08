99ebd62a2171adbdd3ba32189d807aa9de5d1518
Ijay van der Walt <Ijay@accr.co.za>
2020-07-08 16:01:31 +0200

    Add project files.

---
+++ EmailApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmailService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace EmailApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        //Inject IEmailSender interface
+        private readonly IEmailSender _emailSender;
+
+        //Add it to the constuctor
+        public WeatherForecastController(IEmailSender emailSender)
+        {
+            _emailSender = emailSender;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+            var rng = new Random();
+            //Creating a new message object where we pass the recipients, subject and the email body parameters
+            var message = new Message(new string[] { "codemazetest@mailinator.com" }, "Test email async", "This is the content from our async email.", null);
+
+            //Updated for Async Method Of Sending Email
+            await _emailSender.SendEmailAsync(message);
+
+            //Send the message by calling the send email method (synchronously)
+            //_emailSender.SendEmail(message);
+
+
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpPost]
+        public async Task<IEnumerable<WeatherForecast>> Post()
+        {
+            var rng = new Random();
+
+            var files = Request.Form.Files.Any() ? Request.Form.Files : new FormFileCollection();
+
+            var message = new Message(new string[] { "ijay@mailinator.com" }, "Test mail with Attachments", "This is the content from our mail with attachments.", files);
+
+            await _emailSender.SendEmailAsync(message);
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+    }
+}
+
+
---
+++ EmailService/IEmailSender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailService
+{
+    public interface IEmailSender
+    {
+        void SendEmail(Message message);
+
+        //Added for Async Emailing
+        Task SendEmailAsync(Message message);
+        
+    }
+}
